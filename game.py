fd8a48743c0a630f53bd3d33e89c02ce13fe137b
catman1232 <bdcaunt@gmail.com>
2019-05-23 23:10:33 -0400

    Update game.py

--- game.py
+++ game.py
@@ -22,12 +22,15 @@ jacey = species("'Daemon Volantes'", 3, 6, 0, 2, 1, 3, 3, 0, 2, 2, 3, 5, 3, 3, 3
 ethan = species("Neotyrannus Satanus", 4, 2, 0, 2, 2, 6, 1, 2, 3, 4, 1 , 2, 6, 3, 3,4)
 sarah = species("sarah", 3, 6, 0, 4, 3, 2, 1, 0, 2, 3, 2, 6, 3, 4, 5, 4)
 kaleigh = species("kaleigh", 3, 6, 0, 6, 2, 3, 4, 4, 1, 2, 1, 2, 2, 4, 6, 3)
+libby = species("libby", 3, 3, 0, 0, 6, 3, 4, 2, 2, 1, 5, 1, 2, 1, 4, 1)
+aiden = species("aiden", 5, 3, 12, 12, 4, 3, 2, 8, 2, 4, 6, 3, 4, 4, 4, 2)
+shep = species("Shep",4,5,0,6,2,6,4,10,3,2,5,3,2,4,5,6)
 
 #all species in a population
-biosphere = [yeetus,carmen,david,jacey,ethan,sarah,kaleigh]
+biosphere = [yeetus,carmen,david,jacey,ethan,sarah,kaleigh, libby, aiden,shep]
 
 #total in biosphere
-species_num = 7
+species_num = 10
 
 #multiplier applied to each species population when a hurricane event occurs
 hurricane_multi = 0.90
@@ -37,7 +40,7 @@ extinction_multi = 0.01
 extinction_prob = 10000
 
 def main():
-	for generation in range(1):
+	for generation in range(3):
 		#one easy function that reproduces every species while just taking up one line inside the for-loop
 		def reproduce_all():
 			for i in range(species_num):
@@ -57,7 +60,7 @@ def main():
 			#generates random number for hurricane thingy
 			x = []
 			for i in range(0,species_num):
-				x.append(random.uniform(1,0.5)) #¸  <<<<<wut is this character
+				x.append(random.uniform(1,0.5)) #  <<<<<wut is this character
 			#the population is multiplied by the percentage that was defined in the hurricane_multi variable
 			yeetus.population = round(yeetus.population * x[0])
 			carmen.population = round(carmen.population * x[1])
@@ -66,6 +69,9 @@ def main():
 			ethan.population = round(ethan.population * x[4])
 			sarah.population = round(sarah.population * x[5])
 			kaleigh.population = round(kaleigh.population * x[6])
+			libby.population = round(libby.population * x[7])
+			aiden.population = round(aiden.population * x[8])
+			shep.propulation = round(shep.population * x[9])
 			print('a hurricane has occured!!!;  Each species population has been decreased by an average of ~{0}%'.format(round(100-(100*((sum(x)/len(x)))))))
 
 		elif event == 2:
@@ -76,6 +82,9 @@ def main():
 			ethan.population = round(ethan.population * extinction_multi)
 			sarah.population = round(sarah.population * extinction_multis)
 			kaleigh.population = round(kaleigh.population * extinction_multi)
+			libby.population = round(libby.population * extinction_multi)
+			aiden.population = round(aiden.population * extinction_multi)
+			shep.population = round(shep.population * extinction_multi)
 			print('a mass-extinction event has occured; each species population has been decreased by 0.1%')
 		else:
 			pass
@@ -84,8 +93,8 @@ def main():
 		for i in range(1000):
 			#species conflict, see creature.py for more information
 			compete(biosphere)
-
-		mutate(biosphere)
+		for i in range(100):
+			mutate(biosphere)
 
 		population_total = sum([carmen.population, yeetus.population])
 		num_conflict = (0.1)*population_total
@@ -93,26 +102,34 @@ def main():
 		for i in range(species_num):
 			print(f"{biosphere[i].name} has the population of {biosphere[i].population}")
 
-		#prevent extinct species from reproducing by removing them from the biosphere
-		for i in range(species_num):
-			if biosphere[i].population <= 0:
-				biosphere.remove(i)
-			else:
-				pass
+		x = [yeetus.name,carmen.name,david.name,jacey.name,ethan.name,sarah.name,kaleigh.name, libby.name, aiden.name, shep.name]
+		y = [yeetus.population,carmen.population,david.population,jacey.population,ethan.population,sarah.population,kaleigh.population, libby.population, aiden.population, shep.population]
+
+		plt.bar(x, y, color='teal')
+		plt.rcParams.update({'font.size': 20})
+		plt.xlabel("Species'")
+		plt.ylabel("population")
+		plt.rcParams.update({'font.size': 22})
+		plt.title(f"generation {i}")
+
+		plt.xticks(x, x)
+
+		plt.savefig(f"gen_{i}.png")
+
 
 if __name__ == '__main__':
 	main()
 
-	x = [yeetus.name,carmen.name,david.name,jacey.name,ethan.name,sarah.name,kaleigh.name]
-	y = [yeetus.population,carmen.population,david.population,jacey.population,ethan.population,sarah.population,kaleigh.population]
+	x = [yeetus.name,carmen.name,david.name,jacey.name,ethan.name,sarah.name,kaleigh.name, libby.name, aiden.name, shep.name]
+	y = [yeetus.population,carmen.population,david.population,jacey.population,ethan.population,sarah.population,kaleigh.population, libby.population, aiden.population, shep.population]
 
-	plt.bar(x, y, color='green')
-	plt.rcParams.update({'font.size': 10})
+	plt.bar(x, y, color='yellow')
+	plt.rcParams.update({'font.size': 35})
 	plt.xlabel("Species'")
 	plt.ylabel("population")
-	plt.rcParams.update({'font.size': 22})
-	plt.title("Species' and total population")
+	plt.rcParams.update({'font.size': 35})
+	plt.title("Natural selection simulator results (final)")
 
 	plt.xticks(x, x)
-
+	plt.savefig("final.png")
 	plt.show()
